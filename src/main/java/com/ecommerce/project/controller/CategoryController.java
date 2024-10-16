c01047aeff78c99d199ce7d5ca8c29ad5a1c3bc9
Chayan Maiti <chayanmaiti2023@gmail.com>
2024-10-16 12:00:01 +0530

    add ResponceEntity for all endpoint

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
+//    @Autowired
     private CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
@@ -19,13 +21,15 @@ public class CategoryController {
     }
 
     @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return new ResponseEntity<>(categories,HttpStatus.OK);
     }
 
     @PostMapping("/api/public/categories")
-    public String createCategory(@RequestBody Category category) {
-        return categoryService.addCategory(category);
+    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+        categoryService.addCategory(category);
+        return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
